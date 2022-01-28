e9a4587e5c42190c9d9ce132fd00f8e7773cc1cf
Edmund Wu <fangkazuto@gmail.com>
2022-01-28 14:31:08 -0500

    local/loss/ssim: sharpen target image edges
    To provide more structure

--- local/loss/ssim.py
+++ local/loss/ssim.py
@@ -1,4 +1,6 @@
+import torch
 from piqa import SSIM
+from torchvision.transforms.functional import adjust_sharpness
 
 
 class MaximumSSIMLoss(SSIM):
@@ -9,4 +11,6 @@ class MaximumSSIMLoss(SSIM):
           Backpropagation seeks to minimize, so this just conforms
           to it by making the ideal score 0.
         """
+        pred = torch.clip(pred, 0, self.value_range)
+        target = adjust_sharpness(target, sharpness_factor=4)
         return 1. - super().forward(target, pred)
